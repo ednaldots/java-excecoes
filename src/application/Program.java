aa0dfe6d344ac295209fe030f59d081ab5a3dbb5
ednaldo <ednaldo.contato1@gmail.com>
2024-02-28 18:21:02 -0300

    Very bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,26 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		
-			method1();
-				
-			System.out.println("End of program");
-
-		
-	}
-	
-	public static void method1() {
-		System.out.println("***METHOD1 START***");
-		method2();
-		System.out.println("***METHOD1 END***");
-	}
-	
-	public static void method2() {
-		System.out.println("***METHOD2 START***");
-		Scanner sc = new Scanner(System.in);
-		
-		
-		String[] vect = sc.nextLine().split(" ");
-		int position = sc.nextInt();
-		System.out.println(vect[position]);
-		
-		
-		sc.close();
-		System.out.println("***METHOD2 END***");
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
 	}
 }
